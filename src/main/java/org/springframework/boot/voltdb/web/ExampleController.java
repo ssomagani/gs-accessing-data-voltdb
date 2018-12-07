89d2928916a3647792c3a62254836238b119f4b2
ssomagani <seeta.somagani@gmail.com>
2018-12-07 12:28:42 -0500

    Connecting to the JDBC database and performing a simple unique visitor count update

--- src/main/java/org/springframework/boot/voltdb/web/ExampleController.java
+++ src/main/java/org/springframework/boot/voltdb/web/ExampleController.java
@@ -1,5 +1,11 @@
 package org.springframework.boot.voltdb.web;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/")
 public class ExampleController {
-
+	
+	@Autowired
+    JdbcTemplate jdbcTemplate;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public String get() {
-		return System.getenv("VCAP_SERVICES");
-//		return "Root context";
+		jdbcTemplate.execute("update visitors set unique_count = unique_count+1");
+		int countFromDB = jdbcTemplate
+				.query(
+						"select unique_count from visitors", 
+						new SimpleRowMapper()).get(0);
+		
+		return "You are visitor number " + countFromDB;
+	}
+	
+	private class SimpleRowMapper implements RowMapper<Integer> {
+		@Override
+		public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
+			return rs.getInt(1);
+		}
 	}
 }
